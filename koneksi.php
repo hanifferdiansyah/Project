90502bcfe997a24d2e8e90f0692b9180ac9b0db6
hanifferdiansyah <haniffferdiansyah@gmail.com>
2024-11-16 10:37:00 +0700

    proses tambah dan tampil data jurusan

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "prokect_is62";
+$database = "project_is62";
 
 $koneksi = mysqli_connect($host,$username,$password,$database);
 
--- modul-jurusan/index.php
+++ modul-jurusan/index.php
@@ -22,7 +22,7 @@
                 <div class="card">
                     <div class="card-header">
                         <h3 class="float-start">Data Jurusan</h3>
-                        <p class="float-end"><a class="btn btn-primary" href=""><i class="fa-solid fa-square-plus"></i> Tambah Data</a></p>
+                        <p class="float-end"><a class="btn btn-primary" href="form.php"><i class="fa-solid fa-square-plus"></i> Tambah Data</a></p>
                     </div>
                     <div class="card-body">
 
@@ -36,15 +36,33 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php
+                                    #1. Koneksikan file ini
+                                    include("../koneksi.php");
+
+                                    #2. menulis query
+                                    $tampil = "SELECT * FROM jurusans";
+
+                                    #3. jalankan query
+                                    $proses = mysqli_query($koneksi, $tampil);
+
+                                    #4. looping data dari database
+                                    $nomor = 1;
+                                    foreach($proses as $data){
+                                ?>
                                 <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
+                                    <th scope="row"><?=$nomor++?></th>
+                                    <td><?=$data['kode']?></td>
+                                    <td><?=$data['jurusan']?></td>
                                     <td>Otto</td>
                                     <td>
                                         <a class="btn btn-success btn-sm" href=""><i class="fa-solid fa-pen-to-square"></i></a>
                                         <a class="btn btn-danger btn-sm" href=""><i class="fa-solid fa-trash-can"></i></a>
                                     </td>
                                 </tr>
+                                <?php
+                                    }
+                                ?>
                             </tbody>
                         </table>
 
