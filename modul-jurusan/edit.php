704a9e1673c05fa01ccab785d298b6568e29ac53
hanifferdiansyah <haniffferdiansyah@gmail.com>
2024-11-22 15:24:45 +0700

    berhasil edit data

--- modul-jurusan/edit.php
+++ modul-jurusan/edit.php
@@ -44,10 +44,10 @@ $data = mysqli_fetch_array($edit);
                     </div>
                     <div class="card-body">
                         <form action="update.php" method="post">
-                            <input type="hidden" name="id" value="<?$data['id']?>">
+                            <input type="hidden" name="id" value="<?=$data['id']?>">
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Kode Jurusan</label>
-                                <input type="text" readonly ="<?=$data['kode']?>" name="kode" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <input type="text" readonly value="<?=$data['kode']?>" name="kode" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Nama Jurusan</label>
